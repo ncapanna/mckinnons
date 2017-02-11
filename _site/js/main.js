4c3d48fdb9f0d4c42447ba5759d110539ac54cf2
ncapanna <nicolecapanna@Nicoles-MacBook-Pro.local>
2017-02-11 11:29:54 +0800

    Carousel Includes Setup

---
+++ _site/js/main.js
@@ -0,0 +1,25 @@
+
+$(document).ready( function() {
+
+  // Owl Carousel
+  // Basic setup responsive carousel with prev and next
+  $('.owl-carousel').owlCarousel({
+      loop:true,
+      margin:10,
+      nav:true,
+      autoplay:true,
+      responsive:{
+          0:{
+              items:1
+          },
+          600:{
+              items:5
+          },
+          1000:{
+              items:5
+          }
+        }
+  })
+
+});
+// end of document ready
